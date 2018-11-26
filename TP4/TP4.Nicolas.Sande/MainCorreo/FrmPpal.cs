dde6e284e70be5cb01175675717cc7c50578d8a0
PsychoPsyduck <31606266+PsychoPsyduck@users.noreply.github.com>
2018-11-25 21:22:00 -0300

    TP4 - Terminado

---
+++ TP4/TP4.Nicolas.Sande/MainCorreo/FrmPpal.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MainCorreo
+{
+    public partial class RrmPpal : Form
+    {
+
+        private Correo correo;
+        /// <summary>
+        /// 
+        /// </summary>
+        public RrmPpal()
+        {
+            InitializeComponent();
+            this.correo = new Correo();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Paquete paquete = new Paquete(mtxtTrackingID.Text, txtDireccion.Text);
+
+            if (txtDireccion.Text.Length > 0 && txtDireccion.Text.Length < 13 && mtxtTrackingID.Text != "000-000-0000")
+            {
+                paquete.InformaEstado += paq_InformaEstado;
+
+                try
+                {
+                    correo += paquete;
+                }
+                catch (TrackingIdRepetidoException ex)
+                {
+                    MessageBox.Show(ex.Message, "Algo salio mal pinito");
+                }
+
+                this.mtxtTrackingID.Clear();
+                this.txtDireccion.Clear();
+
+                this.ActualizarEstados();
+            }
+            else
+            {
+                MessageBox.Show("Complete los campos");
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<List<Paquete>>((IMostrar<List<Paquete>>)correo);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ActualizarEstados()
+        {
+            lstEstadoIngresado.Items.Clear();
+            lstEstadoEnViaje.Items.Clear();
+            lstEstadoEntregado.Items.Clear();
+
+            foreach (Paquete p in correo.Paquetes)
+            {
+                switch (p.Estado)
+                {
+                    case EEstado.Ingresado:
+                        lstEstadoIngresado.Items.Add(p);
+                        break;
+                    case EEstado.EnViaje:
+                        lstEstadoEnViaje.Items.Add(p);
+                        break;
+                    case EEstado.Entregado:
+                        lstEstadoEntregado.Items.Add(p);
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elemento"></param>
+        private void MostrarInformacion<T>(IMostrar<T> elemento)
+        {
+            if (elemento != null)
+            {
+                Correo correo = (Correo)elemento;
+                string datoAux = "";
+                StringBuilder salida = new StringBuilder();
+
+                foreach(Paquete p in correo.Paquetes)
+                {
+                    datoAux = String.Format("{0} ({1})", p.ToString(), p.Estado.ToString());
+                    salida.AppendLine(datoAux);
+                }
+
+                GuardaString.Guardar(salida.ToString(), "salida.txt");
+                this.rtbMostrar.Text = salida.ToString();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void paq_InformaEstado(Object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                DelegadoEstado d = new DelegadoEstado(paq_InformaEstado);
+                this.Invoke(d, new object[] { sender, e });
+            }
+            else
+            {
+                ActualizarEstados();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mostrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MostrarInformacion<Paquete>((IMostrar<Paquete>)lstEstadoEntregado.SelectedItem);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmPpal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.correo.FinEntregas();
+        }
+        
+    }
+}
---
+++ TP4/TP4.Nicolas.Sande/TestUnitario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestUnitario")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1dc789f1-0cc2-4d61-aaf0-66b1a5f30eb0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
